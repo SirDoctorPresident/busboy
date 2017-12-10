6e8855bc082890118f778870ef66af7614bc6f42
Dominic Roberts <dominic.roberts.c@gmail.com>
2017-12-10 01:50:21 -0500

    Moved scripts and styles into dedicated files

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+document.getElementById('modal-launcher').onclick = function (){
+    modal.open({
+        header: 'The Title',
+        body: `And now here's just text...just some text`,
+    });
+}
 
-app.use('/', express.static(__dirname + '/'));
+document.getElementById('error-launcher').onclick = function (){
+    modal.open({
+        header: 'The Title',
+        headerStyle: 'error-header',
+        body: `And now here's just text...just some text`,
+        icon: 'fa fa-exclamation-circle'
+    });
+}
 
-app.get('/', (req, res) => {
-    res.render('./index.html');
-})
+document.getElementById('info-launcher').onclick = function (){
+    modal.open({
+        header: 'The Title',
+        headerStyle: 'info-header',
+        body: `And now here's just text...just some text`,
+        icon: 'fa fa-info-circle'
+    });
+}
 
-app.listen(port, () => { 
-    console.log('Server running on port ' + port);
-})
+document.getElementById('success-launcher').onclick = function (){
+    modal.open({
+        header: 'The Title',
+        headerStyle: 'success-header',
+        body: `And now here's just text...just some text`,
+        icon: 'fa fa-check-circle-o'
+    });
+}
